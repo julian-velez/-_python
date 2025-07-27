4b5e8800cafd073fb7743b54b70d259a5c9c8817
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-07-27 11:28:20 -0500

    ==_python
    ==_python

---
+++ ==/ejemplo.py
@@ -0,0 +1,6 @@
+# Compara si el número 23 es igual a 23
+# El operador '==' verifica si los dos valores son iguales
+numeros_iguales = 23 == 23  # Esto devuelve True porque 23 sí es igual a 23
+
+# Imprime el resultado de la comparación
+print(numeros_iguales)  # Esto imprimirá_
